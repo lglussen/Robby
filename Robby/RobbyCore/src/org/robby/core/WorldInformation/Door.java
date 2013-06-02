9e77bdc1b8f859ce1c919552bef35845cc632e0b
Dr. Omar Jones <Dr. Omar Jones@192.168.1.130>
2013-06-01 22:21:00 -0400

    Adding Door class to show transitions between areas

---
+++ Robby/RobbyCore/src/org/robby/core/WorldInformation/Door.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.robby.core.WorldInformation;
+
+/**
+ *
+ * @author Dr. Omar Jones
+ */
+public class Door extends Place{
+    
+}
