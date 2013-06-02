b403e8c7f9bdbce738a70814b90a0947653c3d22
Dr. Omar Jones <Dr. Omar Jones@192.168.1.130>
2013-06-01 22:34:31 -0400

    Adding Door class to show transitions between areas

--- Robby/RobbyCore/src/org/robby/core/WorldInformation/Door.java
+++ Robby/RobbyCore/src/org/robby/core/WorldInformation/Door.java
@@ -5,7 +5,8 @@
 package org.robby.core.WorldInformation;
 
 /**
- *
+ * This class represents a point in the level that transitions a player to a 
+ * different area of the game
  * @author Dr. Omar Jones
  */
 public class Door extends Place{
